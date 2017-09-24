999a01a0deafb1b347509928c700364b89481d9b
Natividad Rivera <riveranatividad1@gmail.com>
2017-09-24 15:51:45 -0700

    Update nav.js

--- micro3/nav.js
+++ micro3/nav.js
@@ -1,4 +1,6 @@
 function updatenav() {
+    var page = docuent.getElementByClass("active").innerHTML();
+	console.log(page);
 	document.getElementById("navlist").innerHTML += "<li><a href='../data'>Data</a></li>";
 }
 
