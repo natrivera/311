786bf79a623021b5bbe3644fb47d6255d988d5e7
Natividad Rivera <riveranatividad1@gmail.com>
2017-09-24 14:49:09 -0700

    Delete nav.js

--- micro2/nav.js
+++
@@ -1,8 +0,0 @@
-function updatenav() {
-	console.log("a");
-	document.getElementById("navlist").innerHTML += "<li><a href='/data'>Data</a></li>";
-	console.log("b");
-
-}
-
-window.onLoad = updatenav;
