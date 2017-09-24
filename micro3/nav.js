733ccd9d9887d3771751c655c62a1a1050dfef37
Natividad Rivera <riveranatividad1@gmail.com>
2017-09-24 15:27:40 -0700

    Add files via upload

--- micro3/nav.js
+++ micro3/nav.js
@@ -1,3 +1,6 @@
-	document.getElementById("navlist").innerHTML += "<li><a href='../data'>Data</a></li>";
+function updatenav() {
+	document.getElementById("navlist").innerHTML += "<li><a href="/about">About</a></li>";
 
+}
 
+window.onLoad = updatenav;
